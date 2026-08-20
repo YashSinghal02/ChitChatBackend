2331c95509e8103817d24583534488d52ed8225d
YashSinghal02 <yash20singhal@gmail.com>
2026-08-20 13:37:50 +0530

    signup controller added

--- src/server.js
+++ src/server.js
@@ -3,11 +3,16 @@ import dotenv from "dotenv"
 dotenv.config();
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { dbconnect } from "./lib/db.js";
+import cors from "cors"
 
 const app=express();
+dbconnect();
 
 const PORT=process.env.PORT
 
+app.use(cors())
+app.use(express.json())
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
 
