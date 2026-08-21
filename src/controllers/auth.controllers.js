44b27a24fd71f0943f2dc583a1c0317773197e9e
YashSinghal02 <yash20singhal@gmail.com>
2026-08-21 10:40:52 +0530

    login refractoring

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -73,6 +73,12 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({message:"Email nad Password Are Required"}) ;
+  }
+
+
   try {
     const user = await User.findOne({ email });
     if (!user) {
