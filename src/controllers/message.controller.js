969d4474740f73d02d74b9904468deb3be69a49a
YashSinghal02 <yash20singhal@gmail.com>
2026-08-23 12:47:34 +0530

    message routes optimized

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -55,6 +55,20 @@ export const getAllContacts=async (req,res) => {
        const{text,image}=req.body;
        const {id:receiverId}=req.params;
         const senderId=req.user._id;
+
+        if (!text && !image) {
+      return res.status(400).json({ message: "Text or image is required." });
+    }
+    if (senderId.equals(receiverId)) {
+      return res.status(400).json({ message: "Cannot send messages to yourself." });
+    }
+    // exist:It simply checks whether a matching document exists.
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+      return res.status(404).json({ message: "Receiver not found." });
+    }
+
+
         let imaegUrl;
         if (image) {
             const uploadResponse = await cloudinary.uploader.upload(profilePic);
